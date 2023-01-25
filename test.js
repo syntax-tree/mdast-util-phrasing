9afb1d6c8064f523354462a82a56ab9c5dc96a2d
Titus Wormer <tituswormer@gmail.com>
2023-01-25 11:34:42 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,40 +1,39 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {phrasing} from './index.js'
 
-test('phrasing', (t) => {
-  t.equal(phrasing(), false, 'should return `false` without node')
+test('phrasing', () => {
+  assert.equal(phrasing(), false, 'should return `false` without node')
 
-  t.equal(phrasing(null), false, 'should return `false` with `null`')
+  assert.equal(phrasing(null), false, 'should return `false` with `null`')
 
-  t.equal(
+  assert.equal(
     phrasing({type: 'foo'}),
     false,
     'should return `false` when without known `node`'
   )
 
-  t.equal(
+  assert.equal(
     phrasing({type: 'link'}),
     true,
     'should return `true` when with a phrasing `node`'
   )
 
-  t.equal(
+  assert.equal(
     phrasing({type: 'strong'}),
     true,
     'should return `true` when with another phrasing `node`'
   )
 
-  t.equal(
+  assert.equal(
     phrasing({type: 'paragraph'}),
     false,
     'should return `false` when with a block `node`'
   )
 
-  t.equal(
+  assert.equal(
     phrasing({type: 'list'}),
     false,
     'should return `false` when with another block `node`'
   )
-
-  t.end()
 })
